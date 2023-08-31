a7b7ace78276fc3c34c5b5db5ea9bfc9a7346ccb
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-08-31 20:45:52 -0300

    correção da linha 12

--- aula3at2.py
+++ aula3at2.py
@@ -7,3 +7,12 @@ desconto = "10"
 
 print("Olá, " + cliente + ". Em " + mes + " você realizou uma compra no valor de R$" + valor + 
       " e ganhou um desconto de " + desconto + "%" " em sua próxima compra. Use o cupom " + cupom + ".")
+
+# nome = "Paula"
+# sobrenome = "Martins"
+# mes_compra = "Janeiro"
+# valor_compra = "500.00"
+# valor_desconto = "10"
+# cupom = nome+"É"+valor_desconto
+
+# print("Olá, " + nome + ". Em " + mes_compra + " você realizou uma compra no valor de " + valor_compra + " e ganhou um desconto de " + valor_desconto + "% em sua próxima compra. Use o cupom " + cupom + ".")

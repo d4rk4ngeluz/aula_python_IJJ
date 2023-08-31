80b4b25081a66a536999e4ea131fd530eedcfe0f
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-08-31 20:51:36 -0300

    Feature: correção linha 6

--- aula3at3explic.py
+++ aula3at3explic.py
@@ -3,11 +3,6 @@ sobrenome = input('Digite o seu sobrenome: ')
 mes = 'JANEIRO'
 n1 = int(input('Digite o sua 1 nota: '))
 n2 = int(input('Digite o sua 2 nota: '))
-# desconto = int('10')
-# cupom = nome + "É" + desconto
-# tipo_nome = type(desconto)
-# print(tipo_nome)
 soma = n1 + n2 
 
-print(f" O nome é: {nome} {sobrenome} a primeira nota é {n1} 
-      e a segunda é {n2} a minha nota geral é {soma}")
+print(f" O nome é: {nome} {sobrenome} a primeira nota é {n1} e a segunda é {n2} a minha nota geral é {soma}")

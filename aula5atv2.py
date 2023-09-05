511efbe7a071b77776ee3e78c8b8d1481f821904
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-09-05 20:40:47 -0300

    Feature: Atividade FASHIONSTYLE

---
+++ aula5atv2.py
@@ -0,0 +1,11 @@
+valorCompra = float(input("Informe o valor da compra: R$ "))
+
+# Verificar 30% de Desconto
+if valorCompra >= 500:
+    print("PARABÉNS!!! VOCÊ GANHOU SUPER DESCONTO DE 30% NA FASHIONSTYLE!")
+# Verificar 10% de Desconto
+elif valorCompra >= 250 and valorCompra < 500:
+    print("PARABÉNS. VOCÊ GANHOU 10% DE DESCONTO, MAS PODE GANHAR 30% SE SUA COMPRA FOR ACIMA DE R$500,00")
+# Verificar não tem Desconto
+else:
+    print("POXA, FALTA POUCO PARA VOCÊ GANHAR 10% DE DESCONTO EM SUA COMPRA")

0c85de53d298ab59fe4ae3c00b0504aca6339572
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-09-05 20:28:55 -0300

    Feature: Conceitos de estrutura Python

---
+++ aula4at4.py
@@ -0,0 +1,19 @@
+frutas = ['Maracuja', 'Uva', 'Morango']
+print(30*'_' + "Lista" + '_'*30)
+print(frutas)
+
+automoveis = ('Carro', 'Moto', 'Aviao')
+print(30*'_' + "Tupla" + '_'*30)
+print(automoveis)
+
+pessoa = {
+    "nome": "Renato",
+    "idade": 45,
+    "cidade": "Salvador"
+}
+print(30*'_' + "Dicionário" + '_'*30)
+print(pessoa)
+
+cores = {'azul', 'amarelo', 'rosa'}
+print(30*'_' + "Sets" + '_'*30)
+print(cores)

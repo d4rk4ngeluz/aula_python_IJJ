368e6e71c4c32aa167e664dd21fcc1e2b6d683ce
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-09-05 20:40:02 -0300

    Feature: Atividade habilitação

---
+++ aula5atv1.py
@@ -0,0 +1,11 @@
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print("Parabéns você está apto para tirar sua habilitação")
+
+elif idade >= 17 and idade < 18:
+    print("Você ainda não está apto a dirigir")
+
+else:
+    print("Você não possuem idade mínima para dirigir")
+

0567125b6cb69ff22799ac77d15a95beecac33d9
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-09-05 20:26:28 -0300

    Feature: Atividade média de notas

---
+++ aula4at2.py
@@ -0,0 +1,10 @@
+nome = input('Digite o seu nome: ')
+sobrenome = input('Digite o seu sobrenome: ')
+n1 = float(input('Digite o sua 1 nota: '))
+n2 = float(input('Digite o sua 2 nota: '))
+n3 = float(input('Digite o sua 3 nota: '))
+n4 = float(input('Digite o sua 4 nota: '))
+soma = n1 + n2 + n3 + n4
+media = soma / 4
+print(f" O nome é: {nome} {sobrenome} a primeira nota é {n1} a segunda é {n2} a terceira nota é {n3} a quarta nota é {n4} a minha nota geral é {soma}")
+print(f"Olá,  {nome} ! Sua média é: {media} pontos")

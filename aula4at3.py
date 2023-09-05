7897579ac6e17ebdab69865891ebb3362d695758
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-09-05 20:27:17 -0300

    Feature: Operadores Matemáticos

---
+++ aula4at3.py
@@ -0,0 +1,13 @@
+valor = float(input("Digite o valor a ser calculado: "))
+
+dobro = valor * 2
+triplo = valor * 3
+quadrado = valor **2
+raiz_quadrada = valor**(1/2)
+raiz_cubica = valor**(1/3)
+
+print (f"O dobro do valor é: {dobro}")
+print (f"O triplo do valor é: {triplo}")
+print (f"O quadrado do valor é: {quadrado}")
+print (f"O raiz_quadrada do valor é: {raiz_quadrada}")
+print (f"O raiz_cubica do valor é: {raiz_cubica}")

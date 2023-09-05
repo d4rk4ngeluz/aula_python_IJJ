a901a2f8a94ce9ae06b5c01c31f3940d5369328c
d4rk4ngeluz <rfdsouza@hotmail.com>
2023-09-05 20:25:15 -0300

    Feature: Atividade Pet

---
+++ aula4at1.py
@@ -0,0 +1,6 @@
+nome = input('Digite o seu nome: ')
+idade = int(input("Digite a idade do seu pet: "))
+
+idade_pet = idade * 7
+
+print(f"Olá,  {nome} ! A idade humana do seu pet é: {idade_pet} anos")
